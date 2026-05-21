e8a92a1653196e2e88f56563e3ef0ab18dc036ae
Enekololo <eneko.lasaga@proton.me>
2026-05-21 15:34:15 +0200

    converted to material m3

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// index.js
+// Using a CDN
+import '../node_modules/@material/web/button/filled-button.js';
+import '../node_modules/@material/web/button/outlined-button.js';
+import '../node_modules/@material/web/checkbox/checkbox.js';
+import '../node_modules/@material/web/field/outlined-field.js'
+// Or using a relative path if the files are hosted locally
+// import './node_modules/@material/web/button/filled-button.js';
+// Check if the element is already registered before defining it
+// Replace this:
+import { __assign } from "tslib";
